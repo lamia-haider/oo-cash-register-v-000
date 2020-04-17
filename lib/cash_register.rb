2694f7f6353d7dec8f6e24ae37439e21564bde59
Lamia Haider <lamia.haider@gmail.com>
2020-04-17 05:26:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :discount, :total, :items
-  @cart==[]
+  attr_accessor :discount, :total, :items, 
 
   def initialize(discount=0) #because this is optional we're setting a default value that can be overriden
     @total = 0
     @discount = discount #otherwise discount will be nil
+    @cart = []
   end
 
   def total
