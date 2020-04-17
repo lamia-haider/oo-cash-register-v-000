cbb3ee9b5c5204ceb54aa68db777e8dfdbddd90c
Lamia Haider <lamia.haider@gmail.com>
2020-04-17 04:56:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,5 +3,6 @@ class CashRegister
 
   def initialize(discount=0) #because this is optional we're setting a default value that can be overriden
     @total = 0
+    @discount = discount #otherwise discount will be nil 
   end
 end
