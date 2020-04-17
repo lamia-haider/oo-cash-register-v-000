a914a4267368e78ee4d5eeb1ae18b996b5d7a2f2
Lamia Haider <lamia.haider@gmail.com>
2020-04-17 04:57:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :discount :total
+  attr_accessor :discount, :total
 
   def initialize(discount=0) #because this is optional we're setting a default value that can be overriden
     @total = 0
